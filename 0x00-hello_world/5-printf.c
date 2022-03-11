18b5f80564faf6b6aa5100105607374b8217bfa6
Lady Bash <basiratoyebisi@gmail.com>
2022-03-10 18:51:26 -0800

    v

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 /** 
  * * main - prints the phrase with proper grammar,
  *  * but the outcome is a piece of art, 
@@ -6,7 +6,7 @@ nclude <stdio.h>
  *  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
+
