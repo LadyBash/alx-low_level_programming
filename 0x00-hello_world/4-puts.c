82978444ad72380c83e96433fc258854acd44fa6
Lady Bash <basiratoyebisi@gmail.com>
2022-03-10 17:45:39 -0800

    etty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** 
+ * *main - entry point 
+ * * 
+ * *Return: 0 after printing the fubction 
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
