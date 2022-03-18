311b6fd63f340602e356bb73d95eafd15caf275c
Lady Bash <basiratoyebisi@gmail.com>
2022-03-18 08:06:02 -0700

    v

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,11 +1,11 @@
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c : The character to print
- *
- * Return: On sucess 1.
- * On error, -1 is returned, and error is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+ * Return 0
+ * on error, -1 is returned , and error is set
+ *\
+ int _putchar(char c)
+ {
+ return (write(1, &c, 1));
+ }
+
