6eaebd4c677645ad25717dca3672c4280e84f73a
Lady Bash <basiratoyebisi@gmail.com>
2022-03-28 19:32:07 -0700

    total

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,8 +5,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 void print_diagsums(int *a, int size);
-<<<<<<< HEAD
 void set_string(char **s, char *to);
-=======
->>>>>>> 7f4ce4181890c8595ddf9389a53fb6e50ed31a30
