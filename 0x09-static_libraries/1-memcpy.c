7a8bc777261129d3495056099b5cdccfb1bc3dcd
Lady Bash <basiratoyebisi@gmail.com>
2022-04-01 18:17:52 -0700

    m

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area,
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: bytes filled.
- * Return: the pointer to dest.
- */
+*_memcpy - The _memcpy() function copies n bytes
+* from memory area src to memory area dest
+*@dest:area where bytes are copied to
+*@src:area where bytes are copied from
+*@n:number of bytes to copy
+*Return: returns a pointer to n
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -6,4 +6,8 @@
 */
 int _islower(int c)
 {
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
 }
